516230b9bc8ea4b6aac71f06b8100c36d298c22a
jhionisio <mr.joaomarcelo@gmail.com>
2024-06-28 11:28:41 -0300

    DONE: Adição de atlas mongo conn

---
+++ src/main/java/meta/messengerbot/config/MongoConfig.java
@@ -0,0 +1,34 @@
+package meta.messengerbot.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${mongo.connection.string}")
+    private String connectionStringValue;
+
+    @Bean
+    public MongoClient mongoClient() {
+        String connectionString = connectionStringValue;
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        return MongoClients.create(settings);
+    }
+}
